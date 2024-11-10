4b29b29781a613eb27ffa4a6ea8824a03bf07d3f
Demir Derin <155583484+Demirder@users.noreply.github.com>
2024-11-10 14:41:31 +0300

    Add files via upload

---
+++ 09.11_while_tekrar/while/Program.cs
@@ -0,0 +1,39 @@
+﻿
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random r = new Random();
+            int rast = r.Next(1, 100);
+            int sayac = 5;
+
+
+            while (sayac>0)
+            {
+
+                Console.WriteLine("1 ile 100 arası sayıyı tahmin et");
+                int tahminet = Convert.ToInt32(Console.ReadLine());
+
+                if (rast==tahminet)
+                {
+                    Console.WriteLine("OK");
+                    break;
+                }
+                else if (tahminet > rast)
+                {
+                    Console.WriteLine("Tahmin küçült");
+                }
+                else 
+                {
+                    Console.WriteLine("Tahmin büyült");
+                }
+
+                sayac--;
+            }
+
+        }
+    }
+}
