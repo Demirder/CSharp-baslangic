15276cef24fc63c00aa2aeaf9a77a5d7a28738f2
Demir Derin <155583484+Demirder@users.noreply.github.com>
2024-11-11 13:01:43 +0300

    Add files via upload

---
+++ for_ornekler_10.11/ornekler_10.11/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace ornekler_10._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 20'den 0'a kadar olan sayıları ekrana yazdırınız.
+
+
+            //for (int i = 20; i >= 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            #endregion
+
+            #region kullanıcıdan alınan başlangıç ve bitiş değerlerinin arasındaki sayılardan 3'ün katı olanları ekrana for loop yöntemiyle yazdırır mısın?
+
+            //Console.WriteLine("başlangıç için bir sayı giriniz");
+            //int baslangic = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("bitiş için bir sayı giriniz");
+            //int bitis = Convert.ToInt32(Console.ReadLine());
+
+
+            //for(int i= baslangic; i<=bitis; i++)
+            //{
+
+            //    if (i % 3 == 0)
+            //    {
+
+            //        Console.WriteLine(i);
+
+            //    }
+
+            //}
+
+            #endregion
+
+            #region kullanıcıdan alınan bir sayının faktöriyeli hesaplayan for döngüsü yazın.
+
+            //Console.WriteLine("Faktöriyeli hesaplanacak bir sayı giriniz.");
+            //long sayi = Convert.ToInt32(Console.ReadLine()); //büyük değer girince değer kaybediyor ve eksi görünüyor, bozuk demektir bu da
+
+            //long basla = 1; //değer vermem lazım ki sırayla çarpsın.
+
+            //for (long i = 1; i <= sayi; i++)
+            //{
+
+            //    basla *= i; // sonuc=sonuc*i,     1*1=1 1*2=2 2*3=6 6*4=24.....
+
+            //}
+
+            //Console.WriteLine($"{sayi} sayısının faktöriyel sonucu: {basla}");
+            //Console.WriteLine("faktöriyel:"+basla);
+
+            #endregion
+
+            
+
+        }
+    }
+}
