00e1991f3c66f1ed152d23f5fb5e40bc2fb1db29
Demir Derin <155583484+Demirder@users.noreply.github.com>
2024-11-22 12:28:00 +0300

    Add files via upload

---
+++ 17.11_dizi/17.11_dizi/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _17._11_dizi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] ogrenciler = { "Safiye", "Eymen", "Safiye", "Bekir", "Rümeysa", "Safiye", "Berat", "Safiye" };
+
+            //Yukarıdaki dizinin içinde bulunan SAFİYE isimlerinin Index değerlerini ekrana bir döngü yardımı ile yazdırınız.
+
+            //Düşününki dizi içinde 1Milyon isim olabilir. Oluşturulan döngü tek tek kontrol ederek 1 Milyon tur atmasın. Kodun başarılı olması en az tur da bütün indexleri yazdırmalısınız.
+
+
+            string[] ogrenciler = { "Safiye", "Eymen", "Safiye", "Bekir", "Rümeysa", "Safiye", "Berat", "Safiye" };
+
+            Console.WriteLine("Safiye isminin indexlerini yazdırınız.");
+            int turSayisi =0; //turu sıfırdan başlatıyoruz.
+
+            for (int i = 0; i < ogrenciler.Length; i++) // dizinin uzunluğunu algılamak için lenght kullandım.
+            {
+                turSayisi++;  // Her bir kontrol için tur sayısını artırıyoruz.
+
+                if (ogrenciler[i] == "Safiye")
+                {
+                    Console.WriteLine(i);
+                 }
+
+            }
+
+            Console.WriteLine("Toplam " + turSayisi + " tur atıldı."); //total tur sayısını öğrenmek için de işlemi yapıyoruz.
+
+        }
+    }
+}
